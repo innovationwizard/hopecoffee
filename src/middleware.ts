aa2656265aa6a78371872cdfd61dd77247788a98
Jorge Luis Contreras Herrera <jorgeluiscontrerasherrera@gmail.com>
2026-03-06 13:54:07 -0600

    logo was requiring auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -37,5 +37,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api/auth|_next/static|_next/image|favicon\\.ico|.*\\.png|.*\\.jpg|.*\\.svg|.*\\.ico).*)"],
 };
